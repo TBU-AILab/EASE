d21fd90343c93e18b1fd7b50a2742da63c249518
aviktorin <aviktorin@utb.cz>
2025-03-19 09:54:20 +0100

    UPDATE - cec2022 dim selection

--- fopimt/resource/metabenchmark/cec2022.py
+++ fopimt/resource/metabenchmark/cec2022.py
@@ -14,7 +14,7 @@ class CEC2022(Modul):
 
     @classmethod
     def get_description(cls) -> str:
-        return "CEC 2022 benchmark (https://github.com/P-N-Suganthan/2022-SO-BO) with two options:\nsample - testing sample\nfull - whole benchmark"
+        return "CEC 2022 benchmark (https://github.com/P-N-Suganthan/2022-SO-BO) with two options:\nsample - testing sample\ndim10 - only 10D\ndim20 - only 20D\nfull - whole benchmark"
 
     @staticmethod
     def whole() -> list[dict]:
@@ -23,6 +23,64 @@ class CEC2022(Modul):
         """
 
         dims = [10, 20]
+        funcs = [CEC2022function(funcNum=f_num, dim=dim) for f_num in range(1, 13) for dim in dims]
+        runs = 30
+
+        # Prepare test functions
+        functions = []
+
+        for f in funcs:
+            dim = f.dim
+            if dim == 10:
+                max_fes = 200_000
+            else:
+                max_fes = 1_000_000
+            f_dict = {
+                'func': f,
+                'runs': runs,
+                'dim': dim,
+                'max_fes': max_fes
+            }
+            functions.append(f_dict)
+
+        return functions
+
+    @staticmethod
+    def dim10() -> list[dict]:
+        """
+        Returns the filtered benchmark specification fit for EvaluatorMetaheuristic
+        """
+
+        dims = [10]
+        funcs = [CEC2022function(funcNum=f_num, dim=dim) for f_num in range(1, 13) for dim in dims]
+        runs = 30
+
+        # Prepare test functions
+        functions = []
+
+        for f in funcs:
+            dim = f.dim
+            if dim == 10:
+                max_fes = 200_000
+            else:
+                max_fes = 1_000_000
+            f_dict = {
+                'func': f,
+                'runs': runs,
+                'dim': dim,
+                'max_fes': max_fes
+            }
+            functions.append(f_dict)
+
+        return functions
+
+    @staticmethod
+    def dim20() -> list[dict]:
+        """
+        Returns the filtered benchmark specification fit for EvaluatorMetaheuristic
+        """
+
+        dims = [20]
         funcs = [CEC2022function(funcNum=f_num, dim=dim) for f_num in range(1, 13) for dim in dims]
         runs = 30
 
